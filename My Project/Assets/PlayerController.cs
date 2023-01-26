07b5945dca9db9f89427f183e966a83e00d966e9
Jared Forrest <jared.forrest@xandertalent.com>
2023-01-26 10:53:00 +0000

    fix angle

--- My Project/Assets/PlayerController.cs
+++ My Project/Assets/PlayerController.cs
@@ -59,7 +59,7 @@ public class PlayerController : MonoBehaviour, IDamageable
 
         // Weapon
         Vector2 aimDirection = mousePosition - rb.position;
-        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
         rb.rotation = aimAngle;
     }
 
